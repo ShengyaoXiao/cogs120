5a27f3f8c3f0cde08149619f25f1f3499684cbde
Shengyao Xiao <shx034@ucsd.edu>
2018-10-29 10:24:09 -0700

    add fireabase.json

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Routes from './Routes';
 
 class App extends Component {
   render() {
+    console.log("here");
     return (
       <div>
         <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+console.log("index.js");
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+// registerServiceWroker();
